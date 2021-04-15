6c8ae8a59a6ba5e2d0e2b8229aac91807cac813d
ppp308 <ppp308@naver.com>
2021-04-15 11:20:18 +0900

    valueclass

---
+++ Valueclass.java
@@ -0,0 +1,14 @@
+package Amusementpark;
+
+public class Valueclass {
+	public static final int baby = 0;
+	public static final int day_child = 44000;
+	public static final int day_teen = 47000;
+	public static final int day_adult = 56000;
+	public static final int day_old = 44000;
+	public static final int night_child = 37000;
+	public static final int night_teen = 40000;
+	public static final int night_adult = 46000;
+	public static final int night_old = 37000;
+
+}
