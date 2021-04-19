9f84da80dadf00a5cb0c135320e2d762cfd39df7
ppp308 <ppp308@naver.com>
2021-04-19 11:37:42 +0900

    두번째 프로젝트

---
+++ defnew2/src/Amusementpark/Studentinfo.java
@@ -0,0 +1,11 @@
+package Amusementpark;
+
+public class Studentinfo {
+	public String name;
+	public int id;
+	public int birth;
+	public String address;
+	public double score;
+	public char bloodtype;
+	
+}
